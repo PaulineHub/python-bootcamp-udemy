17c45278b041cc0e9dfa22a5ae7f9611ce04850c
Pauline <huby.pauline@gmail.com>
2022-03-29 18:00:04 -0400

    cours01_bandName

---
+++ 01-bandNameGenerator.py
@@ -0,0 +1,14 @@
+#COURS 1
+
+#print()
+#input("message par defaut apparaisant avt input")
+#nom_variable
+#concaténation de chaines avec +
+#saut de ligne avec \n
+
+#BAND NAME GENERATOR
+
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + city_name + " " + pet_name)
