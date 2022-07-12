fddcad711359c1572031bf52500423b2c85f4b8f
Pauline <huby.pauline@gmail.com>
2022-07-12 18:18:38 -0400

    fix bug gif game us states

---
+++ 25-us-states-game/us-states-game/main.py
@@ -0,0 +1,11 @@
+import turtle
+
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "/home/pauline/projets/python/python_bootcamp/25-us-states-game/us-states-game/blank_states_img.gif"
+screen.bgpic(image)
+screen.addshape(image)
+turtle.shape(image)
+
+screen.exitonclick()
